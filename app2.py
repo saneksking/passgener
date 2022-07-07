c29ed40e79ef84af35316d28c4c8d798ce10f906
Saneks King <saneksking@gmail.com>
2022-07-07 14:34:56 +0700

    Начинаю делать 2 программу для создания паролей

--- app2.py
+++ app2.py
@@ -1,6 +1,7 @@
 import random
 import string
-
+from smartprinter.printers import Printer
+printer = Printer()
 
 class PassGen:
     @staticmethod
@@ -10,17 +11,43 @@ class PassGen:
         return password
 
 
-def main():
-    print('--- Программа для генерации паролей ---')
-    try:
-        pass_length = abs(int(input('Длинна пароля: ')))
-    except ValueError as e:
-        print(e)
-        pass_length = 0
+class Menu:
+    @staticmethod
+    def switch_lang():
+        try:
+            lang = input('Какой язык вы хотите выбрать? rus or eng: ')
+            if lang == 'rus' or lang == 'Rus' or lang == 'RUS':
+                Menu.rus_menu()
+            elif lang == 'eng' or lang == 'Eng' or lang == 'ENG':
+                pass
+    @staticmethod
+    def rus_menu():
+        try:
+            while 1:
+                printer.smart.echo('Программа для создания сложных паролей!', show=True, char='*')
+                printer.smart.echo('От SaneksKing', show=True, char='*')
+                printer.smart.echo('', show=True, char='*')
+                menu = int(input('1: Сгенерировать пароль\n2: Сменить язык\n3: Выход\n: '))
+                if menu == 1:
+                    pass_length = abs(int(input('Длинна пароля: ')))
+                    if pass_length:
+                        password = PassGen.generate(pass_length)
+                        print(password)
+                        printer.smart.echo('Пароль сгенерирован!', show=True, char='*')
+                        cont = input('Нажмите Enter для продолжения или -1 для выхода!: ')
+                        if cont == '-1':
+                            break
+                        else:
+                            continue
+                    else:
+                        print('Вы ввели не число!')
+                        continue
 
-    password = PassGen.generate(pass_length)
-    print(password)
-    print('--- FINISH ---')
+                elif menu == 2:
+
+
+def main():
+    pass
 
 
 if __name__ == '__main__':
